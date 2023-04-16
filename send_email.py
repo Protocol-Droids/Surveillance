7ed655130c0679507e0ce54837f65f8d278867fb
Nettking <m@nettking.no>
2023-04-16 15:23:54 +0200

    Update send_email.py

--- send_email.py
+++ send_email.py
@@ -11,8 +11,11 @@ def send_mail(message='This is a test email.', recipient_email='martin.arthur.an
     msg['To'] = recipient_email
     msg['Subject'] = subject
 
-    # create an SSL/TLS secure connection to the SMTP server
-    context = ssl.create_default_context()
-    with smtplib.SMTP_SSL('smtp.proisp.no', 465, context=context) as smtp:
-        smtp.login(sender_email, 'JabbaJabbaHei1990')
-        smtp.sendmail(sender_email, recipient_email, msg.as_string())
+    # create a regular SMTP connection and upgrade it to a secure connection using starttls()
+    smtp = smtplib.SMTP('smtp.proisp.no', 587)
+    smtp.ehlo()
+    smtp.starttls(context=ssl.create_default_context())
+    smtp.ehlo()
+    smtp.login(sender_email, 'JabbaJabbaHei1990')
+    smtp.sendmail(sender_email, recipient_email, msg.as_string())
+    smtp.quit()
