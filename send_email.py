6aadf751bc232e78ee44180279e1b2896941315d
Nettking <m@nettking.no>
2023-04-16 15:22:53 +0200

    Update send_email.py

--- send_email.py
+++ send_email.py
@@ -1,8 +1,9 @@
 import smtplib
 from email.mime.text import MIMEText
 from email.utils import formataddr
+import ssl
 
-def send_mail(message = 'This is a test email.', recipient_email = 'martin.arthur.andersen@gmail.com', subject = 'Email from bot'):
+def send_mail(message='This is a test email.', recipient_email='martin.arthur.andersen@gmail.com', subject='Email from bot'):
     sender_email = 'bot@nettking.no'
     sender_name = 'Nettking Surveillance Bot'
     msg = MIMEText(message)
@@ -10,10 +11,8 @@ def send_mail(message = 'This is a test email.', recipient_email = 'martin.arthu
     msg['To'] = recipient_email
     msg['Subject'] = subject
 
-    with smtplib.SMTP('smtp.proisp.no', 587) as smtp:
-        smtp.ehlo()
-        smtp.starttls()
-        smtp.ehlo()
+    # create an SSL/TLS secure connection to the SMTP server
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL('smtp.proisp.no', 465, context=context) as smtp:
         smtp.login(sender_email, 'JabbaJabbaHei1990')
         smtp.sendmail(sender_email, recipient_email, msg.as_string())
-        smtp.quit()
