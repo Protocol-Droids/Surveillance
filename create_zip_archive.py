fe7e8024650a203c28d91a732e85b8a8756b829f
Nettking <m@nettking.no>
2023-04-16 16:07:05 +0200

    Update create_zip_archive.py

--- create_zip_archive.py
+++ create_zip_archive.py
@@ -1,15 +1,17 @@
 import os
 import zipfile
 
-def create_zip_archive(zip_file_name='images.zip'):
+def create_zip_archive():
     # get the current working directory
     current_dir = os.getcwd()
 
     # get a list of all the JPG files in the current directory
     jpg_files = [filename for filename in os.listdir(current_dir) if filename.endswith('.jpg')]
 
-    # create a new ZIP archive
-    with zipfile.ZipFile(zip_file_name, 'w', zipfile.ZIP_DEFLATED) as zip_file:
+    # create a new ZIP archive with maximum compression
+    zip_file_name = 'images.zip'
+    compression = zipfile.ZIP_LZMA
+    with zipfile.ZipFile(zip_file_name, 'w', compression) as zip_file:
         # add each JPG file to the archive
         for jpg_file in jpg_files:
             zip_file.write(jpg_file)
