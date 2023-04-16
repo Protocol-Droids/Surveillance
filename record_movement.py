e16646a93c492ce165f751483503680ef2caaf3f
Nettking <m@nettking.no>
2023-04-16 14:48:20 +0200

    Print time diff email delay for debug

--- record_movement.py
+++ record_movement.py
@@ -1,7 +1,6 @@
-import cv2
+from cv2 import VideoCapture, cvtColor, absdiff, threshold, getStructuringElement, dilate, findContours, MORPH_ELLIPSE, THRESH_BINARY, RETR_EXTERNAL, imwrite, imshow, waitKey, destroyAllWindows, CHAIN_APPROX_SIMPLE, COLOR_BGR2GRAY
 import datetime
 import time
-
 import smtplib
 from email.mime.text import MIMEText
 from email.utils import formataddr
@@ -22,11 +21,11 @@ def send_mail(message = 'This is a test email.', recipient_email = 'martin.arthu
         smtp.sendmail(sender_email, recipient_email, msg.as_string())
 
 # open the default camera (index 0)
-cap = cv2.VideoCapture(0)
+cap = VideoCapture(0)
 
 # initialize the first frame
 _, frame1 = cap.read()
-gray1 = cv2.cvtColor(frame1, cv2.COLOR_BGR2GRAY)
+gray1 = cvtColor(frame1, COLOR_BGR2GRAY)
 last_email_time = time.time() # initialize last email time
 while True:
     # get the current date and time
@@ -39,26 +38,27 @@ while True:
         break
     
     # convert the frame to grayscale
-    gray2 = cv2.cvtColor(frame2, cv2.COLOR_BGR2GRAY)
+    gray2 = cvtColor(frame2, COLOR_BGR2GRAY)
     
     # compute the absolute difference between the two frames
-    frame_diff = cv2.absdiff(gray1, gray2)
+    frame_diff = absdiff(gray1, gray2)
     
     # apply thresholding to the frame difference
-    _, thresh = cv2.threshold(frame_diff, 150, 255, cv2.THRESH_BINARY)
+    _, thresh = threshold(frame_diff, 150, 255, THRESH_BINARY)
     
     # dilate the thresholded image to fill in the gaps
-    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
-    dilated = cv2.dilate(thresh, kernel, iterations=5)
+    kernel = getStructuringElement(MORPH_ELLIPSE, (5, 5))
+    dilated = dilate(thresh, kernel, iterations=5)
     
     # find contours in the dilated image
-    contours, _ = cv2.findContours(dilated, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    contours, _ = findContours(dilated, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE)
     
     # if contours are detected, capture the frame as a JPEG image and break the loop
     if len(contours) > 0:
-        cv2.imwrite(new_file_name + ".jpg", frame2)
+        imwrite(new_file_name + ".jpg", frame2)
         #break
         current_time = time.time()
+        print('Time diff email delay: ' + str(current_time - last_email_time))
         if current_time - last_email_time >= 3600:
             send_mail('Movement Detected')
             last_email_time = current_time
@@ -68,11 +68,11 @@ while True:
 
 
     # display the frame
-    cv2.imshow('Preview', frame2)
+    imshow('Preview', frame2)
 
     # wait for key press and exit if 'q' is pressed
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    if waitKey(1) & 0xFF == ord('q'):
         break
 # release the camera and close the window
 cap.release()
-cv2.destroyAllWindows()
+destroyAllWindows()
