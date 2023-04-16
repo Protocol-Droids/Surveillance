c84405f7df3d21f14c3837713662094b79166c6b
Nettking <m@nettking.no>
2023-04-16 18:22:11 +0200

    Update sort_recordings.py

--- sort_recordings.py
+++ sort_recordings.py
@@ -3,30 +3,34 @@ import shutil
 from datetime import datetime
 
 # Change the path to the directory where the files are located
-directory_path = "/Surveillance"
+directory_path = "/path/to/directory"
 
 # Get a list of all the files in the directory
 file_list = os.listdir(directory_path)
 
 # Loop through each file in the list
 for file_name in file_list:
-    # Extract the date and hour from the file name
-    file_name_parts = file_name.split("_")
-    date_str = file_name_parts[0]
-    hour_str = file_name_parts[1]
+    try:
+        # Extract the date and hour from the file name
+        file_name_parts = file_name.split("_")
+        date_str = file_name_parts[0]
+        hour_str = file_name_parts[1]
 
-    # Convert the date and hour strings to datetime objects
-    date = datetime.strptime(date_str, "%Y-%m-%d")
-    hour = datetime.strptime(hour_str, "%H-%M-%S-%f")
+        # Convert the date and hour strings to datetime objects
+        date = datetime.strptime(date_str, "%Y-%m-%d")
+        hour = datetime.strptime(hour_str, "%H-%M-%S-%f")
 
-    # Create the directory path for the day and hour
-    day_path = os.path.join(directory_path, date_str)
-    hour_path = os.path.join(day_path, hour_str)
+        # Create the directory path for the day and hour
+        day_path = os.path.join(directory_path, date_str)
+        hour_path = os.path.join(day_path, hour_str)
 
-    # Create the day and hour directories if they don't exist
-    os.makedirs(hour_path, exist_ok=True)
+        # Create the day and hour directories if they don't exist
+        os.makedirs(hour_path, exist_ok=True)
 
-    # Move the file to the hour directory
-    file_path = os.path.join(directory_path, file_name)
-    new_file_path = os.path.join(hour_path, file_name)
-    shutil.move(file_path, new_file_path)
+        # Move the file to the hour directory
+        file_path = os.path.join(directory_path, file_name)
+        new_file_path = os.path.join(hour_path, file_name)
+        shutil.move(file_path, new_file_path)
+    except ValueError:
+        # Skip files that do not match the expected format
+        print(f"Skipping file {file_name} because it does not match the expected format.")
