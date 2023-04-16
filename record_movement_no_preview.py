ca9d3cf12574f49eff73ab1fb9d3cf1b47f397b6
Nettking <m@nettking.no>
2023-04-16 18:10:06 +0200

    Create record_movement_no_preview.py

---
+++ record_movement_no_preview.py
@@ -0,0 +1,48 @@
+from cv2 import VideoCapture, cvtColor, imshow, waitKey, destroyAllWindows, COLOR_BGR2GRAY
+import datetime
+from detect_motion import detect_motion
+#from create_zip_archive import create_zip_archive
+
+
+# open the default camera (index 0)
+cap = VideoCapture(0)
+
+# initialize the first frame
+_, frame1 = cap.read()
+gray1 = cvtColor(frame1, COLOR_BGR2GRAY)
+threshold_detect_motion=input("Enter threshold for motion detection: ")
+count = 0
+while True:
+    # get the current date and time
+    now = datetime.datetime.now()
+    new_file_name = now.strftime("%Y-%m-%d_%H-%M-%S-%f")
+    
+    # capture a frame
+    ret, frame2 = cap.read()
+    if not ret:
+        print("Error reading the frame")
+        break
+
+    # detect motion
+    detect_motion(frame2, gray1, new_file_name, threshold_detect_motion)
+
+    # display the frame
+    #imshow('Preview', frame2)
+
+    # Refresh comparison frame every 100 frame
+    count += 1
+    if count < 100:
+        count = 0
+        _, frame1 = cap.read()
+        gray1 = cvtColor(frame1, COLOR_BGR2GRAY)
+
+
+    # wait for key press and exit if 'q' is pressed
+    if waitKey(1) & 0xFF == ord('q'):
+        break
+    
+    
+
+# release the camera and close the window
+cap.release()
+destroyAllWindows()
